f0d5f6c2abff24cda97fc56f8939dcbc0e46900e
leeroyjenkins <jabbercloud@cisco.com>
2016-05-12 14:46:28 +0800

    add version \${SDK_BUILD_NUM}

--- Source/Spark.swift
+++ Source/Spark.swift
@@ -5,31 +5,43 @@ import UIKit
 
 public class Spark {
     
-    // TODO: check if it's ok to get sdk version
-    public static let version = "0.9.105"
+    /// The version number of this SDK.
+    public static let version = "0.9.106"
     
+    /// Indicates whether the SDK has been authorized.
     public static func authorized() -> Bool {
         return AuthManager.sharedInstance.authorized()
     }
     
+    /// Deauthorize the SDK. If phone is registered, deregister the phone first.
     public static func deauthorize() {
         AuthManager.sharedInstance.invalidateAccessToken()
     }
     
-    public static func initWith(clientId clientId: String, clientSecret: String, scope: String, redirectUri: String) {
+    /// Initialize the SDK using client id, client secret, scope and redirect URI.
+    /// - parameter clientId: The client id issued when creating your app.
+    /// - parameter clientSecret: The client secret issued when creating your app.
+    /// - parameter scope: Scope defines the level of access that your app requires.
+    /// - parameter redirectUri: Redirect URI, must match one of the URIs provided during app registration.
+    /// - parameter controller: View controller being redirected from and back when during OAuth flow.
+    /// - returns: Void
+    public static func initWith(clientId clientId: String, clientSecret: String, scope: String, redirectUri: String, controller: UIViewController) {
         let authMgr = AuthManager.sharedInstance
         authMgr.setup(clientId: clientId, clientSecret: clientSecret, scope: scope, redirectUri: redirectUri)
+        authMgr.authorizeFromController(controller)
     }
     
+    /// Initialize the SDK using access token directly.
+    /// - parameter clientId: The access token.
+    /// - returns: Void
     public static func initWith(accessToken accessToken: String) {
         let authMgr = AuthManager.sharedInstance
         authMgr.setup(token: accessToken)
     }
-    
-    public static func authorizeFromController(controller: UIViewController) {
-        AuthManager.sharedInstance.authorizeFromController(controller)
-    }
 
+    /// Setup storage plugin to SDK. Different clients may have different storage requirements, for example, security requirements.
+    /// - parameter storage: The storage plugin implemented by client.
+    /// - returns: Void
     public static func setupStorage(storage: Storagable) {
         UserDefaults.sharedInstance = UserDefaults(storage)
     }
@@ -80,6 +92,7 @@ extension Spark {
         return WebhookClient()
     }
 
+    /// Phone allows your app to make media calls on Spark.
     public static var phone: Phone {
         return Phone.sharedInstance
     }
