9eea6eaf39515545d5459592d2d07800c49d0ede
Micah Hainline <micah.hainline@asynchrony.com>
2017-01-10 10:33:30 -0600

    Revert "Uplifted existing TeamTests to use new non-deprecated Spark API"
    This reverts commit f3bc38f62eb8d25bf2c83cf6284209b350938ac7.

--- Tests/TeamTests.swift
+++ Tests/TeamTests.swift
@@ -19,183 +19,182 @@
 // THE SOFTWARE.
 
 import Foundation
-import XCTest
+import Quick
+import Nimble
 @testable import SparkSDK
 
-class XCTeamSpec: XCTestCase {
+class TeamSpec: QuickSpec {
     
-    var spark: Spark!
-    
-    private func validate(team: Team?) {
-        XCTAssertNotNil(team, "Failed request")
-        XCTAssertNotNil(team?.id)
-        XCTAssertNotNil(team?.name)
-        XCTAssertNotNil(team?.created)
+    private func validate(team: Team) {
+        expect(team.id).notTo(beNil())
+        expect(team.name).notTo(beNil())
+        expect(team.created).notTo(beNil())
     }
     
-    override func setUp() {
-        self.spark = Spark(authenticationStrategy: SimpleAuthStrategy(accessToken: Config.selfUser.token!))
-    }
-    
-    func testBasicCreateTeam() {
-        let teamName = "test team"
-        let team = createTeam(teamName: teamName)
-        validate(team: team)
-        XCTAssertEqual(team?.name, teamName)
-    }
-    
-    func testCreateTeamWithNoNameFails() {
-        let team = createTeam(teamName: "")
-        XCTAssertNil(team, "Unexpected successful request")
-    }
-    
-    func testCreateTeamWithSpecialCharacters() {
-        let teamName = "@@@ &&&"
-        let team = createTeam(teamName: teamName)
-        validate(team: team)
-        XCTAssertEqual(team?.name, teamName)
-    }
-    
-    func testBasicListTeams() {
-        let team = createTeam(teamName: "test team")
-        let teams = listTeams()
-        if let teams = teams {
-            XCTAssertGreaterThan(teams.count, 0)
-            XCTAssert(teams.contains() { $0 == team })
-        } else {
-            XCTFail("Failed to list team")
+    override func spec() {
+        beforeSuite {
+            print("Config.selfUser.token: \(Config.selfUser.token)")
+            Spark.initWith(accessToken: Config.selfUser.token!)
         }
-    }
-    
-    func testListTeamsWithMax() {
-        if let teams = listTeams(max: 1) {
-            XCTAssertLessThanOrEqual(teams.count, 1)
-        } else {
-            XCTFail("Failed to list team")
-        }
-    }
-    
-    func testListTeamsWithInvalidMaxFails() {
-        let teams = listTeams(max: -1)
-        XCTAssertNil(teams, "Unexpected successful request")
-    }
-    
-    func testBasicGetTeam() {
-        if let teamCreated = createTeam(teamName: "test team"), let teamId = teamCreated.id, let teamFromGet = getTeam(teamId: teamId) {
-            self.validate(team: teamFromGet)
-            XCTAssertEqual(teamFromGet, teamCreated)
-        } else {
-            XCTFail("Failed to get team")
-        }
-    }
-    
-    func testGetATeamWithInvalidIdFails() {
-        let team = getTeam(teamId: Config.InvalidId)
-        XCTAssertNil(team, "Unexpected successful request")
-    }
-    
-    func testBasicUpdateTeam() {
-        let updatedName = "new test team"
-        if let teamCreated = createTeam(teamName: "test team"), let teamId = teamCreated.id, let teamFromUpdate = updateTeam(teamId: teamId, name: updatedName) {
-            self.validate(team: teamFromUpdate)
-            XCTAssertEqual(teamFromUpdate.id, teamCreated.id)
-            XCTAssertEqual(teamFromUpdate.created, teamCreated.created)
-            XCTAssertEqual(teamFromUpdate.name, updatedName)
-        } else {
-            XCTFail("Failed to update team")
-        }
-    }
-    
-    func testUpdateTeamWithEmptyName() {
-        let updatedName = ""
-        if let teamCreated = createTeam(teamName: "test team"), let teamId = teamCreated.id, let teamFromUpdate = updateTeam(teamId: teamId, name: updatedName) {
-            self.validate(team: teamFromUpdate)
-            XCTAssertEqual(teamFromUpdate.id, teamCreated.id)
-            XCTAssertEqual(teamFromUpdate.created, teamCreated.created)
-            XCTAssertEqual(teamFromUpdate.name, updatedName)
-        } else {
-            XCTFail("Failed to update team")
-        }
-    }
-    
-    func testUpdateTeamWithSpecialName() {
-        let updatedName = "@@@ &&&"
-        if let teamCreated = createTeam(teamName: "test team"), let teamId = teamCreated.id, let teamFromUpdate = updateTeam(teamId: teamId, name: updatedName) {
-            self.validate(team: teamFromUpdate)
-            XCTAssertEqual(teamFromUpdate.id, teamCreated.id)
-            XCTAssertEqual(teamFromUpdate.created, teamCreated.created)
-            XCTAssertEqual(teamFromUpdate.name, updatedName)
-        } else {
-            XCTFail("Failed to update team")
-        }
-    }
-    
-    func testUpdateTeamWithInvalidIdFails() {
-        let team = updateTeam(teamId: Config.InvalidId, name: Config.InvalidId)
-        XCTAssertNil(team, "Unexpected successful request")
-    }
-    
-    func testBasicDeleteTeam() {
-        if let teamCreated = createTeam(teamName: "test team"), let teamId = teamCreated.id {
-            XCTAssertTrue(deleteTeam(teamId: teamId))
-        } else {
-            XCTFail("Failed to create team")
-        }
-    }
-    
-    func testDeleteTeamWithInvalidIdFails() {
-        XCTAssertFalse(deleteTeam(teamId: Config.InvalidId), "Unexpected successful request")
-    }
-
-    @discardableResult
-    private func createTeam(teamName: String) -> Team? {
-        let request = { (completionHandler: @escaping (ServiceResponse<Team>) -> Void) in
-            self.spark.teams.create(name: teamName, completionHandler: completionHandler)
-        }
-        return getResponse(request: request)
-    }
-
-    private func listTeams(max: Int? = nil) -> [Team]? {
-        let request = { (completionHandler: @escaping (ServiceResponse<Array<Team>>) -> Void) in
-            self.spark.teams.list(max: max, completionHandler: completionHandler)
-        }
-        return getResponse(request: request)
-    }
-    
-    private func getTeam(teamId: String) -> Team? {
-        let request = { (completionHandler: @escaping (ServiceResponse<Team>) -> Void) in
-            self.spark.teams.get(teamId: teamId, completionHandler: completionHandler)
-        }
-        return getResponse(request: request)
-    }
-    
-    private func updateTeam(teamId: String, name: String) -> Team? {
-        let request = { (completionHandler: @escaping (ServiceResponse<Team>) -> Void) in
-            self.spark.teams.update(teamId: teamId, name: name, completionHandler: completionHandler)
-        }
-        return getResponse(request: request)
-    }
-    
-    private func deleteTeam(teamId: String) -> Bool {
-        let request = { (completionHandler: @escaping (ServiceResponse<Any>) -> Void) in
-            self.spark.teams.delete(teamId: teamId, completionHandler: completionHandler)
-        }
-        return getResponse(request: request) == nil ? false : true
-    }
-    
-    private func getResponse<T>(request: @escaping (_ completionHandler: @escaping (ServiceResponse<T>) -> Void) -> Void) -> T? {
-        let expect = expectation(description: "Service call")
-        var output: T?
-        request() { response in
-            switch(response.result) {
-            case .success(let result):
-                output = result
-            case .failure(_):
-                output = nil
+        
+        // MARK: - Create a team
+        
+        describe("create a team") {
+            it("normal") {
+                do {
+                    let teamName = "test team"
+                    let team = try Spark.teams.create(name: teamName)
+                    
+                    self.validate(team: team)
+                    expect(team.name).to(equal(teamName))
+                    
+                } catch let error as NSError {
+                    fail("Failed to create team, \(error.localizedFailureReason)")
+                }
             }
-            expect.fulfill()
         }
-        waitForExpectations(timeout: 1) { error in XCTAssertNil(error, "Timeout") }
-        return output
+        
+            it("with empty name") {
+                expect{try Spark.teams.create(name: "")}.to(throwError())
+            }
+            
+            it("with special name") {
+                do {
+                    let teamName = "@@@ &&&"
+                    let team = try Spark.teams.create(name: teamName)
+                    
+                    self.validate(team: team)
+                    expect(team.name).to(equal(teamName))
+                    
+                } catch let error as NSError {
+                    fail("Failed to create team, \(error.localizedFailureReason)")
+            }
+        }
+        
+        // MARK: - List a team
+    
+        describe("list a team") {
+            it("normal") {
+                do {
+                    let team = try Spark.teams.create(name: "test team")
+                    let teams = try Spark.teams.list()
+                    
+                    expect(teams.isEmpty).to(beFalse())
+                    expect(teams.contains(){$0 == team}).to(beTrue())
+                    
+                } catch let error as NSError {
+                    fail("Failed to list team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with max value") {
+                do {
+                    let teams = try Spark.teams.list(max: 1)
+                    expect(teams.count).to(beLessThanOrEqualTo(1))
+                    
+                } catch let error as NSError {
+                    fail("Failed to list team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with invalid max") {
+				expect{try Spark.teams.list(max: -1)}.to(throwError())
+            }
+        }
+        
+        // MARK: - Get a team
+        
+        describe("get a team") {
+            it("normal") {
+                do {
+                    let teamName = "test team"
+                    let team = try Spark.teams.create(name: teamName)
+                    let teamFromGet = try Spark.teams.get(teamId: team.id!)
+                    
+                    self.validate(team: teamFromGet)
+                    expect(teamFromGet).to(equal(team))
+                    
+                } catch let error as NSError {
+                    fail("Failed to get team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with invalid id") {
+                expect{try Spark.teams.get(teamId: Config.InvalidId)}.to(throwError())
+            }
+        }
+        
+        // MARK: - Update a team
+        
+        describe("update a team") {
+            it("normal") {
+                do {
+                    let teamName = "test team"
+                    let team = try Spark.teams.create(name: teamName)
+                    let newTeamName = "new test team"
+                    let teamFromUpdate = try Spark.teams.update(teamId: team.id!, name: newTeamName)
+                    
+                    self.validate(team: teamFromUpdate)
+                    expect(teamFromUpdate.id).to(equal(team.id))
+                    expect(teamFromUpdate.created).to(equal(team.created))
+                    expect(teamFromUpdate.name).to(equal(newTeamName))
+                    
+                } catch let error as NSError {
+                    fail("Failed to update team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with empty name") {
+                do {
+                    let team = try Spark.teams.create(name: "test team")
+                    let teamFromUpdate = try Spark.teams.update(teamId: team.id!, name: "")
+                    
+                    self.validate(team: teamFromUpdate)
+                    expect(teamFromUpdate.id).to(equal(team.id))
+                    expect(teamFromUpdate.created).to(equal(team.created))
+                    expect(teamFromUpdate.name).to(equal(""))
+                    
+                } catch let error as NSError {
+                    fail("Failed to update team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with special name") {
+                do {
+                    let team = try Spark.teams.create(name: "test team")
+                    let teamName = "@@@ &&&"
+                    let teamFromUpdate = try Spark.teams.update(teamId: team.id!, name: teamName)
+                    
+                    self.validate(team: team)
+                    expect(teamFromUpdate.name).to(equal(teamName))
+                    
+                } catch let error as NSError {
+                    fail("Failed to create team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with invalid id") {
+                expect{try Spark.teams.update(teamId: Config.InvalidId, name: Config.InvalidId)}.to(throwError())
+            }
+        }
+        
+        // MARK: - Delete a team
+        
+        describe("delete a team") {
+            it("normal") {
+                do {
+                    let teamName = "test team"
+                    let team = try Spark.teams.create(name: teamName)
+                    expect{try Spark.teams.delete(teamId: team.id!)}.notTo(throwError())
+                    
+                } catch let error as NSError {
+                    fail("Failed to delete team, \(error.localizedFailureReason)")
+                }
+            }
+            
+            it("with invalid id") {
+                expect{try Spark.teams.delete(teamId: Config.InvalidId)}.to(throwError())
+            }
+        }
     }
 }
