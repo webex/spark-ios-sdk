bff911fa3a85d1bbf7129c1bf92611242dcd8ef1
panzh <panzh@cisco.com>
2017-05-04 12:21:42 +0800

    change the different copyright to the same

--- Source/Phone/Call/CallState.swift
+++ Source/Phone/Call/CallState.swift
@@ -1,4 +1,4 @@
-// Copyright 2016 Cisco Systems Inc
+// Copyright 2016-2017 Cisco Systems Inc
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
