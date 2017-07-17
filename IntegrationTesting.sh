a679cbf17cc2e3899149e4d839179e8167e957c9
panzh <panzh@cisco.com>
2017-07-17 18:57:30 +0800

    merge bugfix branch into develop branch

--- IntegrationTesting.sh
+++ IntegrationTesting.sh
@@ -6,6 +6,9 @@ fi
 if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
 	sendemail -f "$Email" -t "$Email" -u "Jenkins build by Travis" -m "BranchName=$TRAVIS_BRANCH\nBuildNumber=$TRAVIS_JOB_ID\nPullRequestNumber=$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
+else
+	echo "not the integration test task return normal..."
+	exit 0
 fi
 
 sleep 60
