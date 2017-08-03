03603e215589bd370c129ef22860352430b75ff8
kliu <kyle.lzy@gmail.com>
2017-08-03 15:28:57 +0800

    Refactor device service

--- Source/Phone/Device/DeviceService.swift
+++ Source/Phone/Device/DeviceService.swift
@@ -28,8 +28,8 @@ struct Device {
     let calliopeDiscoveryServiceUrl: URL
     let metricsServiceUrl: URL
     let deviceType:String
-    var regionCode: String?
-    var countryCode: String?
+    var regionCode: String
+    var countryCode: String
 }
 
 class DeviceService {
@@ -43,27 +43,6 @@ class DeviceService {
     var device: Device?
     
     func registerDevice(phone: Phone, queue: DispatchQueue, completionHandler: @escaping (Result<Device>) -> Void) {
-        
-        let updateRegionHandler: (ServiceResponse<RegionModel>) -> Void = {
-            response in
-            switch response.result {
-            case .success(let model):
-                if let device = self.device,let regionCode = model.regionCode,let countryCode = model.countryCode {
-                    self.device?.regionCode = regionCode
-                    self.device?.countryCode = countryCode
-                    completionHandler(Result.success(device))
-                }
-                else {
-                    let error = SparkError.serviceFailed(code: -7000, reason: "Missing required region info when registering device")
-                    SDKLogger.shared.error("Failed to register device", error: error)
-                    completionHandler(Result.failure(error))
-                }
-            case .failure(let error):
-                SDKLogger.shared.error("Failed to update device region", error: error)
-                completionHandler(Result.failure(error))
-            }
-            
-        }
         
         let registrationHandler: (ServiceResponse<DeviceModel>) -> Void = { response in
             switch response.result {
@@ -80,18 +59,17 @@ class DeviceService {
                     let metricsServiceUrlString = servicesDictionary["metricsServiceUrl"],
                     let metricsServiceUrl = URL(string: metricsServiceUrlString) {
                     
-                    let deviceType: String
-                    if UIDevice.current.userInterfaceIdiom == .pad {
-                        deviceType = "IPAD"
-                    } else if UIDevice.current.userInterfaceIdiom == .phone {
-                        deviceType = "IPHONE"
-                    } else {
-                        deviceType = "UNKNOWN"
+                    self.client.fetchRegion(queue: queue) { regionRes in
+                        var regionCode = "US-WEST";
+                        var countryCode = "US";
+                        if let model = regionRes.result.data, let rc = model.regionCode, let cc = model.countryCode {
+                            regionCode = rc
+                            countryCode = cc
+                        }
+                        let device = Device(phone: phone, deviceUrl: deviceUrl, webSocketUrl: webSocketUrl, locusServiceUrl: locusServiceUrl, calliopeDiscoveryServiceUrl: calliopeDiscoveryServiceUrl, metricsServiceUrl: metricsServiceUrl, deviceType: UIDevice.current.kind, regionCode: regionCode, countryCode: countryCode)
+                        self.device = device
+                        UserDefaults.sharedInstance.deviceUrl = deviceUrlString
                     }
-                    let device = Device(phone: phone, deviceUrl: deviceUrl, webSocketUrl: webSocketUrl, locusServiceUrl: locusServiceUrl, calliopeDiscoveryServiceUrl: calliopeDiscoveryServiceUrl, metricsServiceUrl: metricsServiceUrl,deviceType: deviceType, regionCode: nil,countryCode: nil)
-                    self.device = device
-                    UserDefaults.sharedInstance.deviceUrl = deviceUrlString
-                    self.client.updateRegion(queue: queue, completionHandler: updateRegionHandler)
                     
                 } else {
                     let error = SparkError.serviceFailed(code: -7000, reason: "Missing required URLs when registering device")
@@ -129,5 +107,19 @@ class DeviceService {
         }
         self.device = nil
     }
+}
+
+fileprivate extension UIDevice {
+    
+    var kind: String {
+        if self.userInterfaceIdiom == .pad {
+            return "IPAD"
+        } else if self.userInterfaceIdiom == .phone {
+            return "IPHONE"
+        } else {
+            return "UNKNOWN"
+        }
+    }
+    
 }
 
