1326aa71bf42e33a8f5c9066ae9ea18d4536130a
Rob Peek <rob@pixelcloud.nl>
2017-08-04 12:44:24 +0200

    Added extension to allow the hosting app start an SSO authentication flow.

--- Source/Auth/OAuthAuthenticator.swift
+++ Source/Auth/OAuthAuthenticator.swift
@@ -76,7 +76,6 @@ public class OAuthAuthenticator: Authenticator {
     private var accessTokenCompletionHandlers: [(_ accessToken: String?) -> Void] = []
     private var fetchingAccessTokenInProcess = false
     
-    
     /// The delegate, which gets callbacks for refresh access token failure
     public weak var delegate: OAuthAuthenticatorDelegate?
     
@@ -123,24 +122,8 @@ public class OAuthAuthenticator: Authenticator {
     /// - parameter completionHandler: the completion handler will be called when authentication is complete, with a boolean to indicate if the authentication process was successful. It will be called directly after the OAuth view controller has begun to dismiss itself in an animated way.
     /// - since: 1.2.0
     public func authorize(parentViewController: UIViewController, completionHandler: ((_ success: Bool) -> Void)? = nil) {
-        if let encodedClientId = clientId.encodeQueryParamString,
-            let encodedRedirectUri = redirectUri.encodeQueryParamString,
-            let encodedScope = scope.encodeQueryParamString,
-            let authorizationUrl = URL(string: "https://api.ciscospark.com/v1/authorize?response_type=code"
-            + "&client_id=" + encodedClientId
-            + "&redirect_uri=" + encodedRedirectUri
-            + "&scope=" + encodedScope
-            + "&state=iossdkstate") {
-
+        if let authorizationUrl = self.authorizationUrl() {
             oauthLauncher.launchOAuthViewController(parentViewController: parentViewController, authorizationUrl: authorizationUrl, redirectUri: redirectUri) { oauthCode in
-                
-//                if let oauthCode = oauthCode {
-//                    self.fetchingAccessTokenInProcess = true
-//                    self.oauthClient.fetchAccessTokenFrom(oauthCode: oauthCode, clientId: self.clientId, clientSecret: self.clientSecret, redirectUri: self.redirectUri, completionHandler: self.createAccessTokenHandler(errorHandler: { error in
-//                        SDKLogger.shared.error("Failure retrieving the access token from the oauth code", error: error)
-//                    }))
-//                }
-//                completionHandler?(oauthCode != nil)
                 if let oauthCode = oauthCode {
                     self.fetchingAccessTokenInProcess = true
                     self.oauthClient.fetchAccessTokenFrom(oauthCode: oauthCode, clientId: self.clientId, clientSecret: self.clientSecret, redirectUri: self.redirectUri, completionHandler: { response in
@@ -157,6 +140,20 @@ public class OAuthAuthenticator: Authenticator {
         }
     }
     
+    func authorizationUrl() -> URL? {
+        if let encodedClientId = clientId.encodeQueryParamString,
+           let encodedRedirectUri = redirectUri.encodeQueryParamString,
+           let encodedScope = scope.encodeQueryParamString {
+           return URL(string: "https://api.ciscospark.com/v1/authorize?response_type=code"
+                + "&client_id=" + encodedClientId
+                + "&redirect_uri=" + encodedRedirectUri
+                + "&scope=" + encodedScope
+                + "&state=iossdkstate")
+        }
+        
+        return nil
+    }
+
     /// - see: See Authenticator.accessToken(completionHandler:)
     /// - since: 1.2.0
     public func accessToken(completionHandler: @escaping (String?) -> Void) {
