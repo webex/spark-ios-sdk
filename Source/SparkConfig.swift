4a2f5f997f8b98da2a6fa485ebe912dadb7ea731
qucui <qucui@cisco.com>
2018-03-22 15:45:57 +0800

    [SSDK-1416] Client retry and back-off logic

---
+++ Source/SparkConfig.swift
@@ -0,0 +1,13 @@
+//
+//  SparkConfig.swift
+//  SparkSDK
+//
+//  Created by qucui on 2018/3/22.
+//  Copyright © 2018年 Cisco. All rights reserved.
+//
+
+import UIKit
+
+class SparkConfig: NSObject {
+
+}
